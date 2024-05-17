28aa6768d021a9b107affb62682ecefd1b6d41ac
朵琳 <83268912+Dolyn157@users.noreply.github.com>
2024-05-17 21:49:26 +0800

    v1 to v2 done

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -12,8 +12,8 @@ export interface Creator {
   org?: string
   desc?: string
   links?: SocialEntry[]
-  nameAliases?: string[]
-  emailAliases?: string[]
+  mapByNameAliases?: string[]
+  mapByEmailAliases?: string[]
 }
 
 const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
@@ -29,8 +29,8 @@ export const creators: Creator[] = [
       { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
       { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
     ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
+    mapByNameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
+    mapByEmailAliases: ['neko@ayaka.moe'],
   },
   {
     name: '絢香音',
@@ -42,8 +42,8 @@ export const creators: Creator[] = [
       { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
       { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
     ],
-    nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
-    emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
+    mapByNameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
+    mapByEmailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
   },
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrl(c.username)
