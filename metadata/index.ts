49350c6cb61664d02b97dcc42df6cf611c4d3294
朵琳 <83268912+Dolyn157@users.noreply.github.com>
2024-05-19 23:26:11 +0800

    rename the picfile

--- metadata/index.ts
+++ metadata/index.ts
@@ -4,7 +4,7 @@ export const siteShortName = 'Nólëbase'
 export const siteDescription = '朵琳的乐评一言堂'
 
 /** 文档所在目录 */
-export const include = ['笔记', '生活']
+export const include = ['笔记', '生活','艺术评论']
 
 /** Repo */
 export const githubRepoLink = 'https://github.com/Dolyn157/Dolyn-s-MusicReview'
--- 艺术评论/专辑/重金属/旋律死亡金属/"The Redshift" by Omnium Gatherum.md
+++ 艺术评论/专辑/重金属/旋律死亡金属/"The Redshift" by Omnium Gatherum.md
@@ -8,7 +8,7 @@ tags:
 ## 专辑简介
 
 "The Redshift" 是芬兰旋律死亡金属乐队 Omnium Gatherum 的第四张录音室专辑，由 Candleight Records 在 2008年9月22日发售。这张专辑的歌词都是由在他们的上一张专辑的录制过程中加入的新主唱 Jukka Pelkonen 编写的。
-![专辑封面](./assets/The_Redshift-Omnium_Gatherum.jpeg)
+![专辑封面](./assets/theRedShift.jpg)
 
 ## 曲目
 
